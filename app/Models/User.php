5d382a343a672694973abc3490424566ca5e1397
haohexin <2554470320@qq.com>
2018-05-14 03:19:00 +0000

    Admin permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,15 +76,15 @@ class User extends Authenticatable
         $this->attributes['password'] = $value;
     }
 
-    public function setAvatarAttribute($path)
-    {
-        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-        if ( ! starts_with($path, 'http')) {
-
-            // 拼接完整的 URL
-            $path = "/uploads/images/avatars/$path";
-        }
-
-        $this->attributes['avatar'] = $path;
-    }
+//    public function setAvatarAttribute($path)
+//    {
+//        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+//        if ( ! starts_with($path, 'http')) {
+//
+//            // 拼接完整的 URL
+//            $path = "/uploads/images/avatars/$path";
+//        }
+//
+//        $this->attributes['avatar'] = $path;
+//    }
 }
